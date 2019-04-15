35d397fa1976a7a4f7b15c523d5abda51aa09b20
jho0078 <jho0078@gmail.com>
2019-04-15 06:20:41 +0000

    05_Authentication && 1:N model relation

---
+++ accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .forms import UserForm
+# from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('posts:list')
+        
+    if request.method == 'POST':
+        signup_form = UserCreationForm(request.POST)
+        if signup_form.is_valid():
+            signup_user = signup_form.save()
+            auth_login(request, signup_user)
+            return redirect('posts:list')
+    else:
+        signup_form = UserCreationForm()
+    context = {'signup_form': signup_form}
+    return render(request, 'accounts/signup.html', context)
+    
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('posts:list')
+    
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request, request.POST)
+        if login_form.is_valid():
+            login_user = login_form.get_user()
+            auth_login(request, login_user)
+            return redirect(request.GET.get('next') or 'posts:list')
+    else:
+        login_form = AuthenticationForm()
+    context = {'login_form': login_form}
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:list')
+        
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from .models import Post, Image
 from .forms import PostForm, ImageForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def list(request):
@@ -8,12 +9,15 @@ def list(request):
     context = {'posts': posts,}
     return render(request, 'posts/list.html', context)
 
+@login_required
 def create(request):
     if request.method == 'POST':
         # 업로드 이미지는 request.FILES에 들어있다
         post_form = PostForm(request.POST)
         if post_form.is_valid():
-            post = post_form.save()     #게시글 내용 처리 끝
+            post = post_form.save(commit=False)     #게시글 내용 처리 끝
+            post.user = request.user
+            post.save()
             for image in request.FILES.getlist('file'):
                 request.FILES['file'] = image
                 image_form = ImageForm(files=request.FILES)
@@ -32,9 +36,14 @@ def create(request):
     }
     return render(request, 'posts/form.html', context)
 
-    
+@login_required   
 def update(request, post_pk):
     post = get_object_or_404(Post, pk=post_pk)
+    
+    # 게시글을 작성한 유저와 현재 유저가 다를 경우 수정 불가능
+    if post.user != request.user:
+        return redirect('posts:list')
+    
     if request.method == 'POST':
         post_form = PostForm(request.POST, instance=post)
         if post_form.is_valid():
@@ -49,6 +58,10 @@ def update(request, post_pk):
     
     
 def delete(request, post_pk):
+    
+    if post.user != request.user:
+        return redirect('posts:list')
+    
     post = get_object_or_404(Post, pk=post_pk)
     if request.method == 'POST':
         post.delete()
