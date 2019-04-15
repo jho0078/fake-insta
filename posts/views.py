04ec57ce39dada1c1ac0984f10bdc3c0dc476cbf
jho0078 <jho0078@gmail.com>
2019-04-15 08:43:34 +0000

    06_make Comment

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from .models import Post, Image
-from .forms import PostForm, ImageForm
+from .models import Post, Image, Comment
+from .forms import PostForm, ImageForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def list(request):
     posts = get_list_or_404(Post.objects.order_by('-pk'))
-    context = {'posts': posts,}
+    comment_form = CommentForm()
+    context = {
+        'posts': posts,
+        'comment_form': comment_form,
+    }
     return render(request, 'posts/list.html', context)
 
 @login_required
@@ -66,5 +71,26 @@ def delete(request, post_pk):
     if request.method == 'POST':
         post.delete()
     return redirect('posts:list')
+
+@login_required
+@require_POST
+def comment_create(request, post_pk):
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        # 객체자체를 넣어도 번호를 알아서 가져온다
+        comment.user = request.user
+        # comment.board = board
+        comment.post_id = post_pk
+        comment.save()
+        return redirect('posts:list')
+
+@login_required
+@require_POST      
+def comment_delete(request, post_pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    comment.delete()
+    return redirect('posts:list')
+    
     
             
